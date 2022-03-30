534a08272773ccbbe8f86b9c4bbdc3817b5c7283
Karin Oldbring <karin.oldbring@gmail.com>
2022-03-30 16:30:32 +0000

    Add first basic structure to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// Wait for the DOM to finish loading before running the game
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    // EventListeners for buttons
+
+})
+
+function playGame() {
+
+}
+
+// Players choice
+
+// Computer choice
+
+// Result
