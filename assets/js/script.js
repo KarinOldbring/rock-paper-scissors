ece6b23699685e1c3099d545894e1089ee626636
Karin Oldbring <karin.oldbring@gmail.com>
2022-04-01 10:32:58 +0000

    Add getResult function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,17 @@
 const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
-const result = document.getElementById('result')
+const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
 let randomChoice
 let computerChoice
+let result
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
 userChoice = e.target.id
 userChoiceDisplay.innerHTML = userChoice
 produceComputerChoice()
+getResult()
 
 }))
 
@@ -25,6 +27,25 @@ function produceComputerChoice() {
     if (randomChoice === 2) {
         computerChoice = 'scissors'
     }
-
     computerChoiceDisplay.innerHTML = computerChoice
+}
+
+const getResult = () => {
+    switch (userChoice + computerChoice) {
+        case 'scissorspaper':
+        case 'rockscissors':
+        case 'paperrock':
+            resultDisplay.innerHTML = "Well Done!"
+            break
+        case 'paperscissors':
+        case 'scissorsrock':
+        case 'rockpaper':
+            resultDisplay.innerHTML = "Too bad!"
+            break
+        case 'paperpaper':
+        case 'scissorsscissors':
+        case 'rockrock':
+            resultDisplay.innerHTML = "Even Steven!"
+            break
+    }
 }
