1aed5063bb0182474488d9bdd7baaa9eb9dd8ee6
Karin Oldbring <karin.oldbring@gmail.com>
2022-04-01 09:53:41 +0000

    Add const + let and functions for play game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,30 @@
-const computerChoice = document.getElementById('computer-choice')
-const userChoice = document.getElementById('user-choice')
-const result = document.
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const result = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+let randomChoice
+let computerChoice
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+userChoice = e.target.id
+userChoiceDisplay.innerHTML = userChoice
+produceComputerChoice()
+
+}))
+
+function produceComputerChoice() {
+    const randomChoice = Math.floor(Math.random()*possibleChoices.length)
+
+    if (randomChoice === 0) {
+        computerChoice = 'rock'
+    }
+    if (randomChoice === 1) {
+        computerChoice = 'paper'
+    }
+    if (randomChoice === 2) {
+        computerChoice = 'scissors'
+    }
+
+    computerChoiceDisplay.innerHTML = computerChoice
+}
