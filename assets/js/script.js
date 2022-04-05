9d06d45e501637e98e1a3c447717deff6cc3c67b
Karin Oldbring <karin.oldbring@gmail.com>
2022-04-05 16:53:44 +0000

    Add media queries

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,47 +11,42 @@ const nameInput = document.getElementById('username');
 const nameSubmit = document.getElementById('submit');
 const gameArea = document.getElementById('game-area');
 const nameArea = document.getElementById('name-area');
-const checkUserWinner = document.getElementById('user-score');
-const checkComputerWinner = document.getElementById('computer-score');
 let currentUserScore = document.getElementById('user-score');
 let currentComputerScore = document.getElementById('computer-score');
 let updatedComputerScore = document.getElementById('computer-score');
 let updatedUserScore = document.getElementById('user-score');
 let userChoice = document.getElementById('user-choice');
-let computerChoice = document.getElementById('computer-choice')
-let result = document.getElementById('result');
-
-
+let computerChoice = document.getElementById('computer-choice');
 
 /**
  * user choices
  */
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-    userChoice = e.target.id
-    userChoiceDisplay.innerHTML = userChoice
-    generateComputerChoice()
-    getResult()
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
+    getResult();
 
-}))
+}));
 
 /**
  * generate computer choice
  */
 
 function generateComputerChoice() {
-    const randomChoice = Math.floor(Math.random() * possibleChoices.length)
+    const randomChoice = Math.floor(Math.random() * possibleChoices.length);
 
     if (randomChoice === 0) {
-        computerChoice = 'rock'
+        computerChoice = 'rock';
     }
     if (randomChoice === 1) {
-        computerChoice = 'paper'
+        computerChoice = 'paper';
     }
     if (randomChoice === 2) {
-        computerChoice = 'scissors'
+        computerChoice = 'scissors';
     }
-    computerChoiceDisplay.innerHTML = computerChoice
+    computerChoiceDisplay.innerHTML = computerChoice;
 }
 
 /**
@@ -60,24 +55,24 @@ function generateComputerChoice() {
 
 nameSubmit.addEventListener('click', (e) => {
     if (nameInput.value) {
-        setName()
-        toggleGame()
+        setName();
+        toggleGame();
     } else {
         Swal.fire({
             icon: 'error',
             title: 'Hold it!',
             text: 'Please enter your winning name to play!',
-        })
+        });
     }
-})
+});
 
 function setName() {
-    nameValue.innerText = nameInput.value
+    nameValue.innerText = nameInput.value;
 }
 
 function toggleGame() {
-    nameArea.classList.add('hide')
-    gameArea.classList.remove('hide')
+    nameArea.classList.add('hide');
+    gameArea.classList.remove('hide');
 }
 
 /**
@@ -89,22 +84,22 @@ const getResult = () => {
         case 'scissorspaper':
         case 'rockscissors':
         case 'paperrock':
-            resultDisplay.innerHTML = "Well Done!"
-            incrementUserScore()
-            break
+            resultDisplay.innerHTML = "Well Done!";
+            incrementUserScore();
+            break;
         case 'paperscissors':
         case 'scissorsrock':
         case 'rockpaper':
-            resultDisplay.innerHTML = "Too bad..."
-            incrementComputerScore()
-            break
+            resultDisplay.innerHTML = "Too bad...";
+            incrementComputerScore();
+            break;
         case 'paperpaper':
         case 'scissorsscissors':
         case 'rockrock':
-            resultDisplay.innerHTML = "Even Steven!"
-            break
+            resultDisplay.innerHTML = "Even Steven!";
+            break;
     }
-}
+};
 
 /**
  * increment user-score & check winner
@@ -116,9 +111,9 @@ function incrementUserScore() {
     console.log(updatedUserScore);
     currentUserScore.innerText = updatedUserScore;
     if (updatedUserScore === 5) {
-        Swal.fire('Congratulations you won 🎉')
-        resetScore()
-        resetChoices()
+        Swal.fire('Congratulations you won 🎉');
+        resetScore();
+        resetChoices();
     }
 }
 
@@ -132,9 +127,9 @@ function incrementComputerScore() {
     console.log(updatedComputerScore);
     currentComputerScore.innerText = updatedComputerScore;
     if (updatedComputerScore === 5) {
-        Swal.fire('Sorry you lost, better luck next time 🤗')
-        resetScore()
-        resetChoices()
+        Swal.fire('Sorry you lost, better luck next time 🤗');
+        resetScore();
+        resetChoices();
     }
 }
 
